7a6e8ddb8f160e76685220298a59f31f22217f4a
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-12-22 23:52:43 +0800

    feat(context): add base & new function

--- framework/context.go
+++ framework/context.go
@@ -3,19 +3,117 @@ package framework
 import (
 	"context"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type Context struct {
-	request *http.Request
+	request        *http.Request
 	responseWriter http.ResponseWriter
+	ctx            context.Context
 
+	//time out
+	hasTimeout bool
+
+	//mutex
+	writerMux *sync.Mutex
+}
+
+func NewContext(r *http.Request, w http.ResponseWriter) *Context {
+	return &Context{
+		request:        r,
+		responseWriter: w,
+		ctx:            r.Context(),
+		writerMux:      &sync.Mutex{},
+	}
 
 }
+
+// base 封装基本的函数功能
+
+func (ctx *Context) WriterMux() *sync.Mutex {
+	return ctx.writerMux
+}
+
+func (ctx *Context) GetRequest() *http.Request {
+	return ctx.request
+}
+
+func (ctx *Context) GetResponse() http.ResponseWriter {
+	return ctx.responseWriter
+}
+
+func (ctx *Context) SetHasTimeout() {
+	ctx.hasTimeout = true
+}
+
+// context 标准context接口
 
 func (ctx *Context) BaseContext() context.Context {
 	return ctx.request.Context()
 }
 
+func (ctx *Context) Deadline() (deadline time.Time, ok bool) {
+	return ctx.BaseContext().Deadline()
+}
+
 func (ctx *Context) Done() <-chan struct{} {
 	return ctx.BaseContext().Done()
 }
+
+func (ctx *Context) Err() error {
+	return ctx.BaseContext().Err()
+}
+
+func (ctx *Context) Value(key interface{}) interface{} {
+	return ctx.BaseContext().Value(key)
+}
+
+// end context
+
+// begin request --> http.Request 对外
+
+func (ctx *Context) QueryAll() map[string][]string {
+	if ctx.request != nil {
+		return map[string][]string(ctx.request.URL.Query())
+	}
+	return nil
+}
+
+func (ctx *Context) QueryInt(key string, def int) int {
+	params := ctx.QueryAll()
+	if values, ok := params[key]; ok {
+		len := len(values)
+		if len > 0 {
+			intVal, err := strconv.Atoi(values[len-1])
+			if err != nil {
+				return def
+			}
+			return intVal
+		}
+	}
+	return def
+}
+
+func (ctx *Context) QueryString(key string, def string) string {
+	params := ctx.QueryAll()
+	if values, ok := params[key]; ok {
+		len := len(values)
+		if len > 0 {
+			return values[len-1]
+		}
+	}
+	return def
+}
+
+func (ctx *Context) QueryArray(key string, def []string) []string {
+	params := ctx.QueryAll()
+	if values, ok := params[key]; ok {
+		return values
+	}
+	return def
+}
+
+
+
