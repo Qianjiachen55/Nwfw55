e95b420e704b04bbe1999a6478d09f80a20f85cd
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-12-28 17:42:37 +0800

    add(response.go): init Interface

--- framework/response.go
+++ framework/response.go
@@ -1,1 +1,26 @@
 package framework
+
+
+type IResponse interface {
+	Json(obj interface{}) IResponse
+
+	Jsonp(obj interface{}) IResponse
+
+	Xml(obj interface{}) IResponse
+
+	Html(obj interface{}) IResponse
+
+	Text(obj interface{}) IResponse
+
+	Redirect(obj interface{}) IResponse
+
+	SetHeader(key string,val string) IResponse
+
+	SetCookie(key string,val string,maxAge int,path,domain string,secure,httpOnly bool) IResponse
+
+	SetStatus(code int) IResponse
+
+	SetOkStatus() IResponse
+}
+
+
