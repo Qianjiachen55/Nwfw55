f41b340f3049f15abeff4b6b7661741088557508
Qianjiachen55 <qianjiachen@happywater.xyz>
2021-12-30 14:06:43 +0800

    add(provider.go): add interface

--- framework/provider.go
+++ framework/provider.go
@@ -1,3 +1,16 @@
 package framework
 
+type NewInstance func(...interface{}) (interface{},error)
 
+type ServiceProvider interface {
+
+	Register(Container) NewInstance
+
+	Params(Container) []interface{}
+
+	IsDefer() bool
+
+	Boot(Container) error
+
+	Name() string
+}
